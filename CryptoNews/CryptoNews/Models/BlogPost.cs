24d207c090777742116bba49f0a4936e2716c0e6
John Esan <jcoool40@gmail.com>
2018-01-16 09:53:09 +0100

    Models Added

---
+++ CryptoNews/CryptoNews/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoNews.Models
+{
+    public class BlogPost
+    {
+       
+            public Guid Id { get; set; }
+            public string Link { get; set; }
+            public string Title { get; set; }
+            public string Excerpt { get; set; }
+            public DateTime Date { get; set; }
+            public string FeaturedImage { get; set; }
+            public Guid BlogWebsiteId { get; set; }
+            public virtual BlogWebsite BlogWebsite { get; set; }
+    
+    }
+}
