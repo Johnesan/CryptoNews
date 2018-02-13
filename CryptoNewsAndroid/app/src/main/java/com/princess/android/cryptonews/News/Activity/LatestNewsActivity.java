844cf4b714100ec08034e2fbb12814bd65e47d51
Andy Eshiet <andyeshiet@gmail.com>
2018-02-13 11:52:34 +0100

    Refactored Packages

--- CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/News/Activity/LatestNewsActivity.java
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/News/Activity/LatestNewsActivity.java
@@ -1,4 +1,4 @@
-package com.princess.android.cryptonews;
+package com.princess.android.cryptonews.News.Activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.princess.android.cryptonews.R;
 
 public class LatestNewsActivity extends AppCompatActivity {
 
--- CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/News/Fragment/LatestNewsActivityFragment.java
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/News/Fragment/LatestNewsActivityFragment.java
@@ -1,10 +1,12 @@
-package com.princess.android.cryptonews;
+package com.princess.android.cryptonews.News.Fragment;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.princess.android.cryptonews.R;
 
 /**
  * A placeholder fragment containing a simple view.
