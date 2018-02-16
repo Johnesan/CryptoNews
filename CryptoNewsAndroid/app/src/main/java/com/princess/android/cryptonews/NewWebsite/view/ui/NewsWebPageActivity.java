6b54bda52220b2cbb55efa0c400e12ad0d746267
Andy Eshiet <andyeshiet@gmail.com>
2018-02-16 13:47:31 +0100

    added Settings Activity

--- CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/NewWebsite/view/ui/NewsWebPageActivity.java
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/NewWebsite/view/ui/NewsWebPageActivity.java
@@ -1,9 +1,13 @@
 package com.princess.android.cryptonews.NewWebsite.view.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.princess.android.cryptonews.R;
+import com.princess.android.cryptonews.settings.SettingsActivity;
 
 public class NewsWebPageActivity extends AppCompatActivity {
 
@@ -11,5 +15,29 @@ public class NewsWebPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_web_page);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_latest_news, menu);
+        return  true;
+       }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings){
+            startSettingsActivity();
+            return  true;
+
+        }
+
+        return  super.onOptionsItemSelected(item);
+    }
+
+    private void startSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
