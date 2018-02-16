3cbc1df1372196c303daf8215551b8fa5132be6d
Andy Eshiet <andyeshiet@gmail.com>
2018-02-16 12:27:15 +0100

    modified news Item

---
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/binding/BindingAdapters.java
@@ -0,0 +1,15 @@
+package com.princess.android.cryptonews.binding;
+
+import android.view.View;
+
+/**
+ * Created by numb3rs on 2/16/18.
+ */
+
+public class BindingAdapters {
+    @BindingAdapter("VisibleGone")
+    public static void showHide (View view, boolean show){
+        view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+}
