c745afee449f525dd78cb4ab49d9f7f655988b49
Andy Eshiet <andyeshiet@gmail.com>
2018-03-15 10:02:03 +0100

    Added text Watcher to Url TextView when user make changes after clicking save button

--- CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/settings/fragment/EditWebsitePreferenceFragment.java
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/settings/fragment/EditWebsitePreferenceFragment.java
@@ -10,7 +10,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -191,6 +193,27 @@ public class EditWebsitePreferenceFragment extends DaggerFragment {
                     "Successful",
                     "Your Settings has been successful saved!!",
                     true);
+        });
+
+        //if the user Decides to edit the Edit Text after saving, disable the save button to avoid saving wrong data
+        binding.get().websiteUrl.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //This code disables the save Button
+                binding.get().saveButton.setClickable(false);
+                binding.get().saveButton.setEnabled(false);
+
+            }
         });
 
     }
