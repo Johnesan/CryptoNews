59728af910f4e9fd399043c259035a731196b4eb
damomos <damomos123@gmail.com>
2018-03-29 23:55:19 +0100

    Added Share Functionality

--- CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/newswebsite/view/ui/NewsWebPageActivity.java
+++ CryptoNewsAndroid/app/src/main/java/com/princess/android/cryptonews/newswebsite/view/ui/NewsWebPageActivity.java
@@ -1,5 +1,6 @@
 package com.princess.android.cryptonews.newswebsite.view.ui;
 
+import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,10 @@ import com.princess.android.cryptonews.R;
 
 public class NewsWebPageActivity extends AppCompatActivity {
 
+
+    String title;
+    String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +26,19 @@ public class NewsWebPageActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        if (this.getIntent().hasExtra("title")) {
+            title = this.getIntent().getExtras().getString("title");
+        }
+        if (this.getIntent().hasExtra("url")) {
+            url = this.getIntent().getExtras().getString("url");
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_latest_news, menu);
+        getMenuInflater().inflate(R.menu.menu_news_webpage, menu);
         return true;
     }
 
@@ -34,7 +46,14 @@ public class NewsWebPageActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_share) {
+
+            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            String shareBody = title + " " + url;
+            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+            startActivity(Intent.createChooser(sharingIntent, "Share via"));
 
             return true;
 
